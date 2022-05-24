4cf0a59b414952552a89a40a3933a5d42d0ae81b
tim-hub <tim-hub@users.noreply.github.com>
2022-05-24 14:20:12 +1200

    use them in ticket module

--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -1,4 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { TicketController } from './adapters/api/TicketController';
+import { TicketService } from './domain/ports/TicketService';
+import { TicketApiService } from './adapters/api/TicketAPIService';
+import { TicketRepository } from './domain/ports/TicketRepository';
+import { TicketInMemory } from './adapters/db/TicketInMemory';
 
-@Module({})
+@Module({
+  imports: [Logger],
+  controllers: [TicketController],
+  providers: [
+    TicketService,
+    TicketApiService,
+    {
+      provide: TicketRepository,
+      useClass: TicketInMemory, // can add condition on ENV, inject mock impl for unit testing
+    },
+  ],
+})
 export class TicketModule {}
